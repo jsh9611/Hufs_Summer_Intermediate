43efa3c00741aeaf8ca266adec73c426465fde03
jsh9611 <jangsh9611@naver.com>
2021-08-15 22:43:40 +0900

    commit

---
+++ Intermediate Low/1.basic/단순반복문_보고말하는수열.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = [[] for _ in range(n + 1)]
+	
+arr[1].append(1)
+
+for i in range(1, n):
+    cnt = 1
+    for j in range(1, len(arr[i]) + 1):
+        if j == len(arr[i]) or arr[i][j] != arr[i][j - 1]:
+            arr[i + 1].append(arr[i][j - 1])
+            arr[i + 1].append(cnt)
+            cnt = 1
+        else:
+            cnt += 1
+print(*arr[n])
