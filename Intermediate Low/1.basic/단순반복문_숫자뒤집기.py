32b16a8cd8e8a8ce6164c5868a5fc1dcf283e6cd
jsh9611 <jangsh9611@naver.com>
2021-08-17 22:40:17 +0900

    단순반복문

--- Intermediate Low/1.basic/단순반복문_숫자뒤집기.py
+++ Intermediate Low/1.basic/단순반복문_숫자뒤집기.py
@@ -4,9 +4,14 @@
 # 거꾸로 출력하는 코드를 작성해보세요. (ex : 1234 → 4321)
 # 출처 : 코딩테스트 준비를 위한 알고리즘 정석
 # https://www.codetree.ai/missions/2/concepts/1/problems/square-of-prime-number/description
-num = input()
-for i in range(len(num)-1,-1,-1):
-    if num[i] == '0':
-        continue
-    print(num[i::-1])
-    break
+
+# solved 1
+# num = input()
+# for i in range(len(num)-1,-1,-1):
+#     if num[i] == '0':
+#         continue
+#     print(num[i::-1])
+#     break
+
+# solved 2
+print(int(''.join(reversed(input()))))
